731b905c17bddf35f4b23c043ba5ce3354415355
Steve Kemp <steve@steve.org.uk>
2022-01-08 13:13:05 +0200

    All directory-module to accept multiple arguments

--- modules/module_directory_test.go
+++ modules/module_directory_test.go
@@ -1,8 +1,13 @@
 package modules
 
 import (
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
+
+	"github.com/skx/marionette/environment"
+	"github.com/skx/marionette/file"
 )
 
 func TestDirectoryCheck(t *testing.T) {
@@ -20,20 +25,88 @@ func TestDirectoryCheck(t *testing.T) {
 		t.Fatalf("got error - but wrong one : %s", err)
 	}
 
-	// Wrong kind of target
-	args["target"] = 3
-	err = d.Check(args)
-	if err == nil {
-		t.Fatalf("expected error due to missing target")
-	}
-	if !strings.Contains(err.Error(), "failed to convert") {
-		t.Fatalf("got error - but wrong one : %s", err)
-	}
-
 	// Valid target
 	args["target"] = "/foo/bar"
 	err = d.Check(args)
 	if err != nil {
 		t.Fatalf("unexpected error")
 	}
+}
+
+func TestDirectoryMultiple(t *testing.T) {
+
+	// Create a temporary directory
+	dir, err := os.MkdirTemp("", "m_d_t")
+	if err != nil {
+		t.Fatalf("failed to make temporary directory")
+	}
+
+	// pair of directories we'll create
+	a := filepath.Join(dir, "one")
+	b := filepath.Join(dir, "two")
+
+	// Create a bunch of directories
+	args := make(map[string]interface{})
+	args["target"] = []string{
+		a,
+		b,
+	}
+
+	d := &DirectoryModule{}
+	env := environment.New()
+	changed, err := d.Execute(env, args)
+
+	if err != nil {
+		t.Fatalf("error making multiple directories:%s", err)
+	}
+	if !changed {
+		t.Fatalf("expected to see a change")
+	}
+
+	// Second time around the directories should exist,
+	// so we see no change
+	changed, err = d.Execute(env, args)
+
+	if err != nil {
+		t.Fatalf("error making multiple directories:%s", err)
+	}
+	if changed {
+		t.Fatalf("expected to see no change when directories exist")
+	}
+
+	// Ensure the directories exist
+	if !file.Exists(a) {
+		t.Fatalf("expected to see directory present!")
+	}
+	if !file.Exists(b) {
+		t.Fatalf("expected to see directory present!")
+	}
+
+	// Now remove them
+	args["state"] = "absent"
+	changed, err = d.Execute(env, args)
+
+	if err != nil {
+		t.Fatalf("error removing multiple directories:%s", err)
+	}
+	if !changed {
+		t.Fatalf("expected to see a change when removing directories")
+	}
+
+	if file.Exists(a) {
+		t.Fatalf("expected to see no directory present after removal!")
+	}
+	if file.Exists(b) {
+		t.Fatalf("expected to see no directory present after removal!")
+	}
+
+	// remove them again - should be no change
+	changed, err = d.Execute(env, args)
+	if err != nil {
+		t.Fatalf("error removing multiple directories:%s", err)
+	}
+	if changed {
+		t.Fatalf("expected to see no change when removing absent directories")
+	}
+
 }
