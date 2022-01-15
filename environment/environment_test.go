7f7a3be1573c8be057e470dffda1751dc3c3d446
Steve Kemp <steve@steve.org.uk>
2022-01-15 13:14:49 +0200

    100% test coverage

--- environment/environment_test.go
+++ environment/environment_test.go
@@ -1,8 +1,13 @@
 package environment
 
 import (
+	"os"
 	"runtime"
+	"strings"
 	"testing"
+
+	"github.com/skx/marionette/file"
+	"github.com/skx/marionette/token"
 )
 
 // Test built-in values
@@ -70,4 +75,80 @@ func TestSet(t *testing.T) {
 		t.Fatalf("Wrong value retrieved, after update")
 	}
 
+}
+
+// Test we can get/set environment variables properly
+func TestEnvVariable(t *testing.T) {
+
+	// Create a new object
+	e := New()
+
+	// Set a variable, via the environment
+	os.Setenv("NAME", "world")
+
+	out, err := e.ExpandTokenVariables(token.Token{
+		Type:    token.STRING,
+		Literal: "Hello, ${NAME}",
+	})
+
+	if err != nil {
+		t.Fatalf("unexpected error running expansion: %s\n", out)
+	}
+
+	if out != "Hello, world" {
+		t.Fatalf("Unexpected output: %s", out)
+	}
+
+	// Variables take precedence
+	e.Set("NAME", "Steve")
+
+	out, err = e.ExpandTokenVariables(token.Token{
+		Type:    token.STRING,
+		Literal: "Hello, ${NAME}",
+	})
+
+	if err != nil {
+		t.Fatalf("unexpected error running expansion: %s\n", out)
+	}
+
+	if out != "Hello, Steve" {
+		t.Fatalf("Unexpected output: %s", out)
+	}
+}
+
+// Test we can execute commands to expand tokens
+
+func TestCommandExpansion(t *testing.T) {
+	// Create a new object
+	e := New()
+
+	// Do we have /bin/ls
+	if file.Exists("/bin/ls") && file.Exists("/etc/passwd") {
+
+		out, err := e.ExpandTokenVariables(token.Token{
+			Type:    token.BACKTICK,
+			Literal: "ls /etc/passwd",
+		})
+
+		if err != nil {
+			t.Fatalf("error running ls: %s", err)
+		}
+		if !strings.Contains(out, "passwd") {
+			t.Fatalf("/bin/ls /etc/passwd had weird output:%s", out)
+		}
+	}
+
+	// Test running a command that doesn't exist
+	cmd := "/no/such/file/directory"
+	_, err := e.ExpandTokenVariables(token.Token{
+		Type:    token.BACKTICK,
+		Literal: cmd,
+	})
+
+	if err == nil {
+		t.Fatalf("expected error running missing command, got none")
+	}
+	if !strings.Contains(err.Error(), cmd) {
+		t.Fatalf("expected a decent error message, got %s", err)
+	}
 }
