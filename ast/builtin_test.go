8923ef18b61fef575c5ee0bda1ac6f6ebbf10bb6
Steve Kemp <steve@steve.org.uk>
2022-02-22 06:30:05 +0200

    Added test-coverage of core builtins

--- ast/builtin_test.go
+++ ast/builtin_test.go
@@ -1,6 +1,10 @@
 package ast
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/skx/marionette/file"
+)
 
 func TestFunctionArgs(t *testing.T) {
 
@@ -68,4 +72,146 @@ func TestFunctionArgs(t *testing.T) {
 		}
 	}
 
+}
+
+func TestFunctions(t *testing.T) {
+
+	type TestCase struct {
+		Name   string
+		Input  []string
+		Output Node
+	}
+
+	tests := []TestCase{
+
+		TestCase{Name: "equal",
+			Input: []string{
+				"one",
+				"two",
+			},
+			Output: &Boolean{Value: false},
+		},
+		TestCase{Name: "equal",
+			Input: []string{
+				"one",
+				"one",
+			},
+			Output: &Boolean{Value: true},
+		},
+
+		TestCase{Name: "contains",
+			Input: []string{
+				"cake ",
+				"pie",
+			},
+			Output: &Boolean{Value: false},
+		},
+		TestCase{Name: "contains",
+			Input: []string{
+				"cake",
+				"ake",
+			},
+			Output: &Boolean{Value: true},
+		},
+		TestCase{Name: "empty",
+			Input: []string{
+				"",
+			},
+			Output: &Boolean{Value: true},
+		},
+		TestCase{Name: "empty",
+			Input: []string{
+				"one",
+			},
+			Output: &Boolean{Value: false},
+		},
+		TestCase{Name: "len",
+			Input: []string{
+				"one",
+			},
+			Output: &Number{Value: 3},
+		},
+		TestCase{Name: "len",
+			Input: []string{
+				"steve",
+			},
+			Output: &Number{Value: 5},
+		},
+		TestCase{Name: "md5",
+			Input: []string{
+				"password",
+			},
+			Output: &String{Value: "5f4dcc3b5aa765d61d8327deb882cf99"},
+		},
+		TestCase{Name: "sha1sum",
+			Input: []string{
+				"secret",
+			},
+			Output: &String{Value: "e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4"},
+		},
+		TestCase{Name: "upper",
+			Input: []string{
+				"one",
+			},
+			Output: &String{Value: "ONE"},
+		},
+		TestCase{Name: "lower",
+			Input: []string{
+				"OnE",
+			},
+			Output: &String{Value: "one"},
+		},
+		TestCase{Name: "set",
+			Input: []string{
+				"OnE",
+			},
+			Output: &Boolean{Value: true},
+		},
+		TestCase{Name: "set",
+			Input: []string{
+				"",
+			},
+			Output: &Boolean{Value: false},
+		},
+	}
+
+	if file.Exists("/etc/passwd") {
+		tests = append(tests,
+			TestCase{Name: "exists",
+				Input: []string{
+					"/etc/passwd",
+				},
+				Output: &Boolean{Value: true},
+			})
+		tests = append(tests,
+			TestCase{Name: "exists",
+				Input: []string{
+					"/etc/passwd.passwd/blah",
+				},
+				Output: &Boolean{Value: false},
+			},
+		)
+	}
+	for _, test := range tests {
+
+		// Find the function
+		fun, ok := FUNCTIONS[test.Name]
+		if !ok {
+			t.Fatalf("failed to find test")
+		}
+
+		// Call the function
+		ret, err := fun(nil, test.Input)
+		if err != nil {
+			t.Fatalf("error calling %s(%v) %s", test.Name, test.Input, err)
+		}
+
+		// Compare the results
+		a := test.Output.String()
+		b := ret.String()
+
+		if a != b {
+			t.Fatalf("error running test %s(%v) - %s != %s", test.Name, test.Input, a, b)
+		}
+	}
 }
