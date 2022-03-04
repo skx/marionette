0a7aaae451a6e81fbf5518547d868038305914b5
Steve Kemp <steve@steve.org.uk>
2022-03-04 17:26:06 +0200

    Added test-case for `Config` package.
    This is a simple addition, because I'm trying to improve
    test-coverage of things as part of this change.

--- config/config.go
+++ config/config.go
@@ -4,6 +4,8 @@
 // and made available to all our plugins.
 package config
 
+import "fmt"
+
 // Config holds the state which is set by the main driver, and is
 // made available to all of our plugins.
 type Config struct {
@@ -15,4 +17,12 @@ type Config struct {
 	// Verbose is used to let our plugins know that the marionette
 	// CLI was started with the `-verbose` flag present.
 	Verbose bool
+}
+
+// String converts this object to a string, only used for the test-case.
+func (c *Config) String() string {
+	if c == nil {
+		return "Config{nil}"
+	}
+	return fmt.Sprintf("Config{Debug:%t Verbose:%t}", c.Debug, c.Verbose)
 }
